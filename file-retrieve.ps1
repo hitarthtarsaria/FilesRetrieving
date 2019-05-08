cb26a4f03ec152f78a4efaa1cae916d461852a0a
Hitarth Tarsaria <31856733+hitarthtarsaria@users.noreply.github.com>
2019-05-08 10:54:33 -0400

    Update file-retrieve.ps1

--- file-retrieve.ps1
+++ file-retrieve.ps1
@@ -5,8 +5,11 @@ param (
     [string]$clientSecret = $OctopusParameters["Name-of-Parameter"]
 )
 
+#Checking the Powershell Version
+
 Write-Host "Powershell Version: $($PSVersionTable.PSVersion)"
 
+# Getting AZ Module and installing package
 if (!(Get-Module "Az")) {
     Install-PackageProvider -Name NuGet -Force
     Install-Module -Name Az -AllowClobber -Force
@@ -19,16 +22,17 @@ $credential = New-Object System.Management.Automation.PSCredential($clientId, $p
 Connect-AzAccount -Tenant $tenantId -Subscription $subscriptionId -ServicePrincipal -Credential $credential -Confirm:$false -WarningAction SilentlyContinue | Out-Null
 
 
-
+#Passing the Directory file path
 
 $SourceFile = "directory"
 $Files = Get-ChildItem $SourceFile
-$Guid = New-Guid 
+$Guid = New-Guid  // Creating a temporary with GUID
 $Destination = New-Item -Path "Z:\" -Name $Guid -ItemType "directory" 
 Write-Host " Destination Name"
 Write-Output $Destination
 Copy-Item $Files -Destination $Destination
 
+#Passing your resources
 
 $location = "location-of-your-resource"
 $resourceGroup = "ResourceGroupName"
@@ -36,11 +40,12 @@ $storageAccountName = "StorageAccountName"
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroup -Name $storageAccountName
 $ctx = $storageAccount.Context
 $containerName = "ContainerName"
+
 # upload a file
 
 
 foreach ( $x in $Files) {
-    Write-Output $x
+    Write-Output $x // To check the content we are fetching it
     $targetPath = $tenant+ "_" + $x.Name 
     Set-AzStorageBlobContent -File $x.fullname -Container $containerName -Blob $targetPath -Context $ctx 
 }
